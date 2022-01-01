3cacd5c2e065e9bd1fc699871eac35c0424d319f
RikeshThapa <rthapa@u.rochester.edu>
2022-01-01 15:58:28 -0800

    adding tailwind configs. integrating tailwind and formatting main page. Adding video component.

--- bo-boilerplate/pages/index.js
+++ bo-boilerplate/pages/index.js
@@ -1,60 +1,62 @@
-import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>BitOverflow</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <video className="background-video" autoPlay loop>
-          <source src="/videos/background0.mp4" />
+    <div>
+      <header className="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+        <div className="flex flex-col">
+          <div className="relative z-30 text-white text-6xl font-bold bg-transparent">
+              BitOverflow
+          </div>
+          <div className="relative z-30 text-white text-6xl font-bold bg-transparent">
+              Built Different.
+          </div>
+        </div>
+        <video autoPlay loop muted className="absolute z-10 w-auto min-w-full min-h-full max-w-none" >
+          <source src="/videos/background2-block-0.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
         </video>
-        <h1 className="title">
-          BitOverflow
-        </h1>
+        <link rel="icon" href="/favicon.ico" />
+      </header>
 
-        <h2>
-          Built Different.
-        </h2>
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>VerdeBlocks &rarr;</h3>
-            <p className="description">
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          <a href="" 
+          className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+            <h3 className="text-2xl font-bold">VerdeBlocks &rarr;</h3>
+            <p className="mt-4 text-xl">
               <code>The future of energy</code>
             </p>
             <p>A wholesale electric trading platform utilizing decentralized infrastructure to ensure instant, secure, guaranteed payment upon delivery.
             </p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Defungable &rarr;</h3>
-            <p className="description">
+          <a href="" 
+          className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+            <h3 className="text-2xl font-bold">Defungable &rarr;</h3>
+            <p className="mt-4 text-xl">
               <code>The Thanos Gauntlet for NFTs</code>
             </p>
             <p>DeFunged puts creators, artists, musicians, fans and community at the center of the NFT universe.</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
+            href=""
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3>BudOverflow &rarr;</h3>
-            <p className="description">
+            <h3 className="text-2xl font-bold">BudOverflow &rarr;</h3>
+            <p className="mt-4 text-xl">
               <code>Crafting, curating and cultivating cannabis</code>
             </p>
             <p>Find the cannabis for you. Research backed. BudOverflow is the ultimate tool to exploring the world of cannabis</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
+            href=""
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3>0_o &rarr;</h3>
-            <p className="description">
+            <h3 className="text-2xl font-bold">0_o &rarr;</h3>
+            <p className="mt-4 text-xl">
               <code>A new frontier for healthcare</code>
             </p>
             <p>
@@ -64,169 +66,9 @@ export default function Home() {
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .background-video {
-          height: 100%;
-          width: 100%;
-          float: left;
-          top: 0;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
