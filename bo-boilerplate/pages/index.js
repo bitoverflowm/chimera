430b6915d037eae26f3837d7cd5568991774bd03
RikeshThapa <rthapa@u.rochester.edu>
2022-01-24 11:25:21 -0800

    adding proper route for verdeblocks

--- bo-boilerplate/pages/index.js
+++ bo-boilerplate/pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
             </div>
         </div>    
         <div className="flex flex-wrap items-center justify-around max-w-7xl mt-6 sm:w-full">
-          <Link href="www.verdeblocks.com">
-            <a href="" 
+          <Link href="https://www.verdeblocks.com/">
+            <a 
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
               <Emoji_Engine label="verdeblocks"/>
               <h3 className="text-2xl font-bold">VerdeBlocks &rarr;</h3>
