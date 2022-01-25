85b05669ddf44b410ea1e687ab329c6004438d83
RikeshThapa <rthapa@u.rochester.edu>
2022-01-24 20:28:35 -0800

    Adding google analytics

--- bo-boilerplate/pages/_app.js
+++ bo-boilerplate/pages/_app.js
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as ga from '../lib/ga';
+
+import '../styles/globals.css';
+
 
 function App({ Component, pageProps }){
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            ga.pageview(url)
+        }
+        //subscribe to router changes and log page views
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        //If the conponent is unmountedd, unsubscribe
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return <Component {...pageProps} />
 }
 
